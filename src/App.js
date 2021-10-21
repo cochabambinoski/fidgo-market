14f943ac987e47df028324219f2a14eeaa475568
Feber Castellon <cochabambinoski@gmail.com>
2021-10-21 10:05:22 -0400

    first card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
       <h1>Esto es Fidgo Market</h1>
+      <Product/>
     </div>
   );
 }
