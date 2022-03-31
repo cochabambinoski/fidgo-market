cbfc6e2d5d498ca5ff0af547bf27be12542cc9e4
Feber <cochabambinoski@gmail.com>
2022-03-31 18:26:36 -0400

    adding basic data

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Product from './Product';
+import products from '../product-data'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -17,18 +18,13 @@ export default function Products() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Product/>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Product/>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Product/>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Product/>
-        </Grid>
+        {
+          products.map(product=>(
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <Product key={product.id} product={product}/>
+            </Grid>
+          ))
+        }
       </Grid>
     </Box>
   );
---
+++ src/product-data.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: 1,
+        name: "AF1",
+        productType: "Wear Shoes",
+        price: 50,
+        rating: 5,
+        image:
+            "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f094af40-f82f-4fb9-a246-e031bf6fc411/calzado-air-force-1-07-GCkPzr.png",
+        description:
+            "El fulgor vive en Nike Air Force 1 ’07, el ícono del básquetbol que le da un toque fresco a las características más recordadas: colores audaces y la cantidad perfecta de destellos para que brilles."
+    },
+    {
+        id: 2,
+        name: "Vaper 90",
+        productType: "Running Shoes",
+        price: 67,
+        rating: 4,
+        image:
+            "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/9251fa0f-2014-482b-81d2-f98972fbed89/air-vapormax-2021-fk-mens-shoes-NpTfFz.png",
+        description:
+            "Put an ultra-light bounce in your step. The heel-to-toe VaporMax Air gives you lofty cushioning, and the Flyknit wraps your foot in flexible, airy support. The speckled outsole, made with recycled Nike Grind materials, gives you durable traction."
+    },
+    {
+        id: 3,
+        name: "Run all day",
+        productType: "Day Shoes",
+        price: 120,
+        rating: 3,
+        image:
+            "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/c8a6efcf-dfd1-4e50-a5de-9cae9044bf57/react-infinity-run-flyknit-2-mens-road-running-shoes-DttDF2.png",
+        description:
+            "The Nike React Infinity Run Flyknit 2 continues to help keep you running. A refreshed Flyknit upper uses Flywire technology for support and breathability where you need it. Higher foam provides soft responsiveness and long-lasting comfort, plus a wider forefoot helps lessen the chance of stress-related injuries. It's still one of our most tested shoes, designed to help you feel the potential when your foot hits the pavement."
+    }
+];
+
+export default products;
